e0b7b71be85c84b75fc8977f11e8ea401ebb4f4b
gtroja <gtroja@users.noreply.github.com>
2019-10-02 01:06:50 -0300

    olá velho projeto
    fiquei deprimido e fugi da realidade fuçando no render do projeto. gostei das bobagens q fiz
    - desfoco faz o ambiente vibrar
    - movimento vertical ondula ambiente

--- src/EntradaSaida.cpp
+++ src/EntradaSaida.cpp
@@ -17,7 +17,7 @@ int EntradaSaida::inicializa(){
 		fprintf(stderr, "Falhei em iniciar a sdl\n");
 		return 0;
 	}
-	if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY,"1")){
+	if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY,"0")){
 		fprintf(stderr, "Aviso! algo estranho aconteceu com a sdl hint (eu sla qq isso significa, na real n muda nada)\n");
 	}
 	//inicia a janela
@@ -177,24 +177,24 @@ void EntradaSaida::atualizaCamera(){
 	
 	// Esses troço ai fazem o jodador ficar em diferentes posições na tela de ocordo com a posição do jogador na fase;
 	
-	float offx = (float)(seguindo->getPosicao().x) / ((float)(fase->getTerreno()->getTamanhoX()));
-	offx = offx*janelaLargura*0.5;
-	offx -= jogador->getPosicao().z*((1/(float)2)-(jogador->getPosicao().x/(float)terreno->getTamanhoX()))*40;
+	// float offx = (float)(seguindo->getPosicao().x) / ((float)(fase->getTerreno()->getTamanhoX()));
+	// offx = offx*janelaLargura*0.5;
+	// offx -= jogador->getPosicao().z*((1/(float)2)-(jogador->getPosicao().x/(float)terreno->getTamanhoX()))*40;
 
-	float offy = (jogador->getPosicao().z*(((jogador->getPosicao().y/(float)terreno->getPico()))*16));
-	offy += (float)(seguindo->getPosicao().y) / ((float)(fase->getTerreno()->getPico()))*(janelaAltura*0.7);
-	offy += 20;
-	novaPos.x = seguindo->getPosicao().x-offx;
-	novaPos.y = seguindo->getPosicao().y-offy;
+	// float offy = (jogador->getPosicao().z*(((jogador->getPosicao().y/(float)terreno->getPico()))*16));
+	// offy += (float)(seguindo->getPosicao().y) / ((float)(fase->getTerreno()->getPico()))*(janelaAltura*0.7);
+	// offy += 20;
+	// novaPos.x = seguindo->getPosicao().x-offx;
+	// novaPos.y = seguindo->getPosicao().y-offy;
 
-	novaPos.z = seguindo->getPosicao().z-24; 
+	// novaPos.z = seguindo->getPosicao().z-24; 
 
 	//opção mais simples
-	// float off = (float)(seguindo->getPosicao().x) / (float)(fase->getTerreno()->getTamanhoX());
-	// off = off*janelaLargura;
+	float off = (float)(seguindo->getPosicao().x) / (float)(fase->getTerreno()->getTamanhoX());
+	off = off*janelaLargura;
 
-	// novaPos.x = seguindo->getPosicao().x-off;
-	// novaPos.y = seguindo->getPosicao().y-200;
+	novaPos.x = seguindo->getPosicao().x-off;
+	novaPos.y = seguindo->getPosicao().y-200;
 
 	camera.setPosicao(novaPos);
 	
@@ -210,11 +210,11 @@ void EntradaSaida::atualizaCamera(){
 	#define DEBUG 0
 	//DEBUG }
 }
-/*
+
 //RENDER 3D :O
 int EntradaSaida::renderiza(){
 
-	int perspectiva = 24;
+	int perspectiva = 16;
 
 	d("entrei no renderiza");
 
@@ -265,20 +265,27 @@ int EntradaSaida::renderiza(){
 		SDL_SetRenderTarget( render, textura );
 		SDL_SetRenderDrawColor(render, 0, 0, 0, 0);
 		SDL_RenderClear(render);
-
+			int distanceFromPlayer = jogador->getPosicao().z - i;
+		fprintf(stderr,"distancia do jogador %i\n", distanceFromPlayer);
+		
 		//setar a cor e offset;
-		SDL_SetRenderDrawColor( render, 20+(200/nroPlanos)*i ,20+(255/nroPlanos)*i, 20+(255/nroPlanos)*i, 255);
+		SDL_SetRenderDrawColor( render, 20+(200/nroPlanos)*i ,20+(255/nroPlanos)*i , 20+(255/nroPlanos)*i, 255);
 		
 
 		offsetX = perspectiva*(nroPlanos-i);
 
 		//offsetX =  -2*i; //jogador->getPosicao().z*i; //um 3d loco :)
 		offsetY = perspectiva*(nroPlanos-i);
+
 	
 		//define o tamanho da cavas:
 		canvas.h = camera.getTamanho().y+offsetY;
 		canvas.w = camera.getTamanho().x+offsetX;
 
+		if(distanceFromPlayer != 0){
+			canvas.h += rand() % distanceFromPlayer;
+			canvas.w += rand() % distanceFromPlayer;
+		}
 		d("canvas size: (%i,%i)",canvas.w,canvas.h)
 
 		//encontra em que posição do terreno começa a desenhar ()
@@ -315,14 +322,20 @@ int EntradaSaida::renderiza(){
 		quadrado.x = 0;
 		quadrado.y = 0-(iniY*multY);
 
+
 		
 		//desenha o terreno na textura:
 		for(j=iniX; j<fimX; j++){
 
+			int meio = abs(distanceFromPlayer) > 3 ? distanceFromPlayer : 1;
+			meio *= jogador->getVelocidade().y/10;
+			
 			quadrado.h = terreno->getYRaw(j,i)*multY;
+			if(meio != 0){quadrado.h += rand() % meio;} 
 
 			if(quadrado.h > 0){
 				quadrado.h -= (iniY*multY)+(iniY*multY)*-1;//evita flics 
+				
 				SDL_RenderFillRect( render, &quadrado);
 			}	
 
@@ -362,182 +375,182 @@ int EntradaSaida::renderiza(){
 	SDL_RenderPresent(render);
 	SDL_Delay(1000/30);
 }
-*/
 
 
 
-int EntradaSaida::renderiza(){
 
-	d("entrei no renderiza");
+// int EntradaSaida::renderiza(){
 
-	//limpo a tela
-	SDL_SetRenderTarget( render, NULL );
-	SDL_SetRenderDrawColor(render, 0, 0, 0, 0 );
-	SDL_RenderClear(render);
+// 	d("entrei no renderiza");
 
-	//seto a textura, para eu trabalhar nela, e limpo
-	SDL_SetRenderTarget( render, textura);
-	SDL_SetRenderDrawColor(render, 0, 0, 0, 0 );
-	SDL_RenderClear(render);
+// 	//limpo a tela
+// 	SDL_SetRenderTarget( render, NULL );
+// 	SDL_SetRenderDrawColor(render, 0, 0, 0, 0 );
+// 	SDL_RenderClear(render);
 
-	Terreno* terreno = fase->getTerreno();
+// 	//seto a textura, para eu trabalhar nela, e limpo
+// 	SDL_SetRenderTarget( render, textura);
+// 	SDL_SetRenderDrawColor(render, 0, 0, 0, 0 );
+// 	SDL_RenderClear(render);
 
-	int i,j,k;
-	int r,g,b;
-	int offsetX,offsetY;
-	int multX, multY, multZ;
-	int nroPlanos = /*terreno->getTamanhoZRaw(); ///*/camera.getTamanho().z;//not today :(
-	int iniX,iniY,fimX,fimY;
-	SDL_Rect canvas;
-	SDL_Rect dest;
+// 	Terreno* terreno = fase->getTerreno();
 
-	canvas.x = 0;
-	canvas.y = 0;
-	canvas.h = janelaAltura;
-	canvas.w = janelaLargura;
+// 	int i,j,k;
+// 	int r,g,b;
+// 	int offsetX,offsetY;
+// 	int multX, multY, multZ;
+// 	int nroPlanos = /*terreno->getTamanhoZRaw(); ///*/camera.getTamanho().z;//not today :(
+// 	int iniX,iniY,fimX,fimY;
+// 	SDL_Rect canvas;
+// 	SDL_Rect dest;
 
-
-
-	//Material m = terreno.getMaterial();
+// 	canvas.x = 0;
+// 	canvas.y = 0;
+// 	canvas.h = janelaAltura;
+// 	canvas.w = janelaLargura;
 
 
-	multX = terreno->getMultiplicador().x;
-	multY = terreno->getMultiplicador().y;
-	multZ = terreno->getMultiplicador().z;
 
-	atualizaCamera();
-	d("seguido = (%i,%i)",seguindo->getPosicao().x, seguindo->getPosicao().y)
-	d("camera = (%i,%i)",camera.getPosicao().x, camera.getPosicao().y);
-	d("irei renderizar %i planos", nroPlanos);
-	d("camera é de tamando(%i,%i), canvas(%i,%i)",camera.getTamanho().x,camera.getTamanho().y,canvas.w,canvas.h)
+// 	//Material m = terreno.getMaterial();
 
-	//loop que desenha no render
-	for(i=camera.getPosicao().z;i<=camera.getPosicao().z+nroPlanos;i++){// varre cada plano xy;
 
-		//setar a cor e offset;
-		r = (200/nroPlanos)*(i-camera.getPosicao().z);
-		g = (255/nroPlanos)*(i-camera.getPosicao().z);
-		b = (255/nroPlanos)*(i-camera.getPosicao().z);
+// 	multX = terreno->getMultiplicador().x;
+// 	multY = terreno->getMultiplicador().y;
+// 	multZ = terreno->getMultiplicador().z;
 
-		//if(jogador->getPosicao().z == i) r+=20;//realça o plano que o usuario está
-		SDL_SetRenderDrawColor( render,r,g,b, 255);
+// 	atualizaCamera();
+// 	d("seguido = (%i,%i)",seguindo->getPosicao().x, seguindo->getPosicao().y)
+// 	d("camera = (%i,%i)",camera.getPosicao().x, camera.getPosicao().y);
+// 	d("irei renderizar %i planos", nroPlanos);
+// 	d("camera é de tamando(%i,%i), canvas(%i,%i)",camera.getTamanho().x,camera.getTamanho().y,canvas.w,canvas.h)
+
+// 	//loop que desenha no render
+// 	for(i=camera.getPosicao().z;i<=camera.getPosicao().z+nroPlanos;i++){// varre cada plano xy;
+
+// 		//setar a cor e offset;
+// 		r = (200/nroPlanos)*(i-camera.getPosicao().z);
+// 		g = (255/nroPlanos)*(i-camera.getPosicao().z);
+// 		b = (255/nroPlanos)*(i-camera.getPosicao().z);
+
+// 		//if(jogador->getPosicao().z == i) r+=20;//realça o plano que o usuario está
+// 		SDL_SetRenderDrawColor( render,r,g,b, 255);
 		
 
 
 
-		offsetX = ((1/(float)2)-(jogador->getPosicao().x/(float)terreno->getTamanhoX()))*40*i; // DA UM EFEITO 3D
-		offsetY = ((jogador->getPosicao().y/(float)terreno->getPico()))*-16*i;	//DA UM EFEITO 3D
-		//offsetX = -2*i; // fica isometrico, é mais facil de prever onde o jogador ficar se mudar de plano 
-		//offsetY = -8*i; // isometrico
+// 		offsetX = ((1/(float)2)-(jogador->getPosicao().x/(float)terreno->getTamanhoX()))*40*i; // DA UM EFEITO 3D
+// 		offsetY = ((jogador->getPosicao().y/(float)terreno->getPico()))*-16*i;	//DA UM EFEITO 3D
+// 		//offsetX = -2*i; // fica isometrico, é mais facil de prever onde o jogador ficar se mudar de plano 
+// 		//offsetY = -8*i; // isometrico
 	
-		//define o tamanho da cavas:
+// 		//define o tamanho da cavas:
 		
-		d("canvas size: (%i,%i)",canvas.w,canvas.h)
+// 		d("canvas size: (%i,%i)",canvas.w,canvas.h)
 
-		//encontra em que posição do terreno começa a desenhar ()
-		iniX = camera.getPosicao().x;
-		iniY = camera.getPosicao().y;
-		d("iniTRU:(%i,%i)",iniX,iniY)
-
-
-		fimX = iniX + camera.getTamanho().x + multX*nroPlanos;//é arbitrario, mas é pro offset não quebrar a imagem
-		fimY = iniY + camera.getTamanho().y + multY*nroPlanos;
-		d("fimTRU:(%i,%i)",fimX,fimY)
+// 		//encontra em que posição do terreno começa a desenhar ()
+// 		iniX = camera.getPosicao().x;
+// 		iniY = camera.getPosicao().y;
+// 		d("iniTRU:(%i,%i)",iniX,iniY)
 
 
-		iniX /= multX; // como cada pixel de terreno representa multx pixels printados, descubro qualpegar;
-		iniY /= multY;
-		d("iniPIX:(%i,%i)",iniX,iniY)
-
-		fimX /= multX;		
-		fimY /= multY;
-
-		iniX = 0;
-		iniY = 0;
-
-		fimY = terreno->getPico();
-		fimX = terreno->getTamanhoXRaw();
-
-		// iniX-=20;
-		// iniY-=20;
-
-		// fimX+=10;
-		// fimY+=10;
-
-		// iniX e Y marcam as posições reais do inicio da tela render
+// 		fimX = iniX + camera.getTamanho().x + multX*nroPlanos;//é arbitrario, mas é pro offset não quebrar a imagem
+// 		fimY = iniY + camera.getTamanho().y + multY*nroPlanos;
+// 		d("fimTRU:(%i,%i)",fimX,fimY)
 
 
-		//define o tamanho de um pixel printado;
-		quadrado.h = multY;
-		quadrado.w = multX;
-		quadrado.x = offsetX;
-		quadrado.y = 0-(iniY*multY)+offsetY;
+// 		iniX /= multX; // como cada pixel de terreno representa multx pixels printados, descubro qualpegar;
+// 		iniY /= multY;
+// 		d("iniPIX:(%i,%i)",iniX,iniY)
 
-		
-		//desenha o terreno na textura:
-		for(j=iniX; j<fimX; j++){
+// 		fimX /= multX;		
+// 		fimY /= multY;
 
-			// linha guia
-			// if(jogador->getPosicao().x-j*multX < multX && jogador->getPosicao().x-j*multX >= 0)
-			// SDL_SetRenderDrawColor( render,r+20,g,b, 255);
+// 		iniX = 0;
+// 		iniY = 0;
 
-			quadrado.h = terreno->getYRaw(j,i)*multY;
+// 		fimY = terreno->getPico();
+// 		fimX = terreno->getTamanhoXRaw();
 
-			if(quadrado.h > 0){
-				quadrado.h -= (iniY*multY)+(iniY*multY)*-1;//evita flics 
-				SDL_RenderFillRect( render, &quadrado);
-			}
-			// if(jogador->getPosicao().x-j*multX < multX && jogador->getPosicao().x-j*multX >= 0)
-			// SDL_SetRenderDrawColor( render,r,g,b, 255);	
+// 		// iniX-=20;
+// 		// iniY-=20;
 
-			quadrado.x+=multX;
+// 		// fimX+=10;
+// 		// fimY+=10;
 
-		}
+// 		// iniX e Y marcam as posições reais do inicio da tela render
+
+
+// 		//define o tamanho de um pixel printado;
+// 		quadrado.h = multY;
+// 		quadrado.w = multX;
+// 		quadrado.x = offsetX;
+// 		quadrado.y = 0-(iniY*multY)+offsetY;
 
 		
+// 		//desenha o terreno na textura:
+// 		for(j=iniX; j<fimX; j++){
 
-		iniX *= multX;//posição real printada;
-		iniY *= multY;//posição real printada;
+// 			// linha guia
+// 			// if(jogador->getPosicao().x-j*multX < multX && jogador->getPosicao().x-j*multX >= 0)
+// 			// SDL_SetRenderDrawColor( render,r+20,g,b, 255);
 
-		//define a posição do canvas:
+// 			quadrado.h = terreno->getYRaw(j,i)*multY;
 
-			canvas.x = camera.getPosicao().x - iniX;
+// 			if(quadrado.h > 0){
+// 				quadrado.h -= (iniY*multY)+(iniY*multY)*-1;//evita flics 
+// 				SDL_RenderFillRect( render, &quadrado);
+// 			}
+// 			// if(jogador->getPosicao().x-j*multX < multX && jogador->getPosicao().x-j*multX >= 0)
+// 			// SDL_SetRenderDrawColor( render,r,g,b, 255);	
 
-			canvas.y = camera.getPosicao().y - iniY;
+// 			quadrado.x+=multX;
 
-		/*
-		//mostra uma linha guia
-		SDL_SetRenderDrawColor( render,r+60,g,b, 255);
-		SDL_RenderDrawLine( render, jogador->getPosicao().x - iniX + offsetX, 0,  jogador->getPosicao().x - iniX + offsetX, terreno->getY( jogador->getPosicao().x,i)+offsetY+1-iniY);
-		SDL_SetRenderDrawColor( render,r,g,b, 255);
-		*/
+// 		}
 
-		//verifica se o player está neste plano
-		if( jogador->getPosicao().z == i){
-			//printa ele
-			//SDL_SetRenderDrawColor( render, 100+(255/nroPlanos)*i ,(200/nroPlanos)*i, (200/nroPlanos)*i, 255);
-			quadrado.x = jogador->getPosicao().x - iniX + offsetX;
-			quadrado.y = jogador->getPosicao().y - iniY + offsetY;
-			quadrado.w = jogador->getTamanho().x;
-			quadrado.h = jogador->getTamanho().y;
-			SDL_RenderFillRect( render, &quadrado);
-		}
+		
 
-	}
+// 		iniX *= multX;//posição real printada;
+// 		iniY *= multY;//posição real printada;
 
-	if(canvas.x < 0) canvas.x = 0;
-	if(canvas.y < 0) canvas.y = 0;
+// 		//define a posição do canvas:
 
-	SDL_SetRenderTarget( render, NULL );
+// 			canvas.x = camera.getPosicao().x - iniX;
 
-	SDL_RenderCopyEx( render, textura, &canvas, NULL, 0, NULL, (SDL_RendererFlip) (SDL_FLIP_VERTICAL) );
+// 			canvas.y = camera.getPosicao().y - iniY;
+
+// 		/*
+// 		//mostra uma linha guia
+// 		SDL_SetRenderDrawColor( render,r+60,g,b, 255);
+// 		SDL_RenderDrawLine( render, jogador->getPosicao().x - iniX + offsetX, 0,  jogador->getPosicao().x - iniX + offsetX, terreno->getY( jogador->getPosicao().x,i)+offsetY+1-iniY);
+// 		SDL_SetRenderDrawColor( render,r,g,b, 255);
+// 		*/
+
+// 		//verifica se o player está neste plano
+// 		if( jogador->getPosicao().z == i){
+// 			//printa ele
+// 			//SDL_SetRenderDrawColor( render, 100+(255/nroPlanos)*i ,(200/nroPlanos)*i, (200/nroPlanos)*i, 255);
+// 			quadrado.x = jogador->getPosicao().x - iniX + offsetX;
+// 			quadrado.y = jogador->getPosicao().y - iniY + offsetY;
+// 			quadrado.w = jogador->getTamanho().x;
+// 			quadrado.h = jogador->getTamanho().y;
+// 			SDL_RenderFillRect( render, &quadrado);
+// 		}
+
+// 	}
+
+// 	if(canvas.x < 0) canvas.x = 0;
+// 	if(canvas.y < 0) canvas.y = 0;
+
+// 	SDL_SetRenderTarget( render, NULL );
+
+// 	SDL_RenderCopyEx( render, textura, &canvas, NULL, 0, NULL, (SDL_RendererFlip) (SDL_FLIP_VERTICAL) );
 	
-	SDL_RenderPresent(render);
-	#define DEBUG 1
-	d("tempo passado: %i", tempoPassado)
+// 	SDL_RenderPresent(render);
+// 	#define DEBUG 1
+// 	d("tempo passado: %i", tempoPassado)
 
-	SDL_Delay(1000/30);
-}
+// 	SDL_Delay(1000/30);
+// }
 
 
 /*
